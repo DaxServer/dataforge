7bedcf701f6958bc79e0403f7f290931030e3ad5
DaxServer <7479937+DaxServer@users.noreply.github.com>
2025-06-13 00:02:09 +0200

    fix: use advanced mode of file upload component (#19)

--- frontend/src/pages/CreateProject.vue
+++ frontend/src/pages/CreateProject.vue
@@ -1,15 +1,11 @@
 <script setup lang="ts">
 const store = useCreateProjectStore()
-const { selectedFile, isCreating, message } = storeToRefs(store)
+const { message } = storeToRefs(store)
 
 // Cleanup store state when component is unmounted
 onUnmounted(() => {
   store.resetState()
 })
-
-const handleAdvancedUpload = () => {
-  store.createProject()
-}
 </script>
 
 <template>
@@ -21,25 +17,26 @@ const handleAdvancedUpload = () => {
         </template>
 
         <template #content>
-          <div class="space-y-6">
+          <div class="">
             <div>
               <label class="block text-sm font-medium text-gray-700 mb-2">
                 Upload Data File
               </label>
               <FileUpload
                 name="file"
-                :customUpload="true"
-                @uploader="handleAdvancedUpload"
-                :multiple="false"
                 accept=".json"
+                customUpload
                 @select="store.handleFileSelect"
                 @remove="store.clearFile"
                 @clear="store.clearFile"
-                class="w-full"
+                @uploader="store.createProject"
+                :fileLimit=1
+                :previewWidth=0
+                :chooseButtonProps="{ severity: 'info' }"
               >
                 <template #empty>
                   <div class="flex items-center justify-center flex-col p-6">
-                    <i class="pi pi-cloud-upload !border-2 !rounded-full !p-8 !text-4xl !text-muted-color" />
+                    <i class="pi pi-cloud-upload border-1 rounded-full p-6 !text-4xl text-info" />
                     <p class="mt-6 mb-0 text-gray-600">Drag and drop files to here to upload.</p>
                     <p class="text-xs text-gray-500 mt-2">
                       JSON files only
@@ -47,20 +44,6 @@ const handleAdvancedUpload = () => {
                   </div>
                 </template>
               </FileUpload>
-
-              <div v-if="selectedFile" class="mt-3">
-                <Message severity="info" :closable="false">
-                  <template #icon>
-                    <i class="pi pi-file"></i>
-                  </template>
-                  <div class="flex items-center justify-between w-full">
-                    <span>{{ selectedFile.name }}</span>
-                    <span class="text-xs">
-                      {{ (selectedFile.size / 1024 / 1024).toFixed(2) }} MB
-                    </span>
-                  </div>
-                </Message>
-              </div>
             </div>
           </div>
 
