9119ee2cd6b6eb0895118c3cb38a2d5101315245
DaxServer <7479937+DaxServer@users.noreply.github.com>
2025-06-15 19:35:03 +0200

    testing: drop non-working frontend testing (#30)

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,5 @@
 import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
-import { fileURLToPath, URL } from 'node:url'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { PrimeVueResolver } from '@primevue/auto-import-resolver'
@@ -15,11 +14,6 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 export default defineConfig({
-  // resolve: {
-  //   alias: {
-  //     '@': fileURLToPath(new URL('./src', import.meta.url)),
-  //   },
-  // },
   plugins: [
     VueDevTools(),
     vue(),
